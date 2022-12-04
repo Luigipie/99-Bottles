2904d8a25fa2fb9d0cab1a6d033568e415e3d228
Luigipie <119540578+Luigipie@users.noreply.github.com>
2022-12-04 11:12:57 +0100

    Add files via upload

---
+++ 99 Bottles.py
@@ -0,0 +1,9 @@
+counter = 99
+while counter > 1:
+    print (counter , 'Bottles on the wall,' , counter , 'bottles of beer, take one e pass it' ,  counter , 'bottles of beer on the wall')
+    counter -= 1
+    if counter == 1:
+        print (counter , "bottle of beer on the wall," , counter , "bottle of beer, take one e pass it,There are no more beer bottles on the wall.")
+        print ("\n There are no more bottles of beer on the wall, no more bottles of beer. "
+               "\n Go to the shop and buy more,  99 bottles of beer on the wall")
+               
